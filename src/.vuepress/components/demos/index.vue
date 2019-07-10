8d8c78bab98711e71cebd4a1a2dfff1f7ba89e49
deulos <ragnar@deulos.com>
2019-07-10 13:04:52 +0200

    Add documentation index demo

---
+++ src/.vuepress/components/demos/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <vue-flux
+    :options="options"
+    :images="images"
+    :transitions="transitions"
+    :captions="captions"
+    style="width: 480px; height: 360px;">
+
+    <template v-slot:preloader>
+      <flux-preloader></flux-preloader>
+    </template>
+
+    <template v-slot:caption>
+      <flux-caption></flux-caption>
+    </template>
+
+    <template v-slot:controls>
+      <flux-controls></flux-controls>
+    </template>
+
+    <template v-slot:pagination>
+      <flux-pagination></flux-pagination>
+    </template>
+  </vue-flux>
+</template>
+
+<script>
+  import {
+    VueFlux,
+    FluxPreloader,
+    FluxCaption,
+    FluxControls,
+    FluxPagination,
+  } from 'vue-flux';
+
+  export default {
+    components: {
+      VueFlux,
+      FluxPreloader,
+      FluxCaption,
+      FluxControls,
+      FluxPagination,
+    },
+
+    data: () => ({
+      options: {
+        allowFullscreen: true,
+        autoplay: true,
+        lazyLoadAfter: 5,
+        path: 'img/slides/',
+      },
+      images: [],
+      transitions: [
+        'fade', 'cube', 'book', 'wave', 'round2',
+      ],
+      captions: [],
+    }),
+
+    created() {
+      this.loadImages();
+    },
+
+    methods: {
+      loadImages() {
+        let srcs = [];
+
+        for (let i = 1; i <= 60; i++) {
+          srcs.push(i.toString().padStart(2, '0') +'.jpg');
+        }
+
+        this.images = [];
+        this.captions = [];
+
+        let index, src;
+
+        for (let i = 1; i <= 16; i++) {
+          index = Math.floor(Math.random() * srcs.length);
+          src = srcs.splice(index, 1)[0];
+
+          this.captions.push(`This is the image number ${i}`);
+          this.images.push(src);
+        }
+      },
+    }
+  };
+</script>
