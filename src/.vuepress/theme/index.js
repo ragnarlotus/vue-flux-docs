93cc12eba3c131c68479a6d996f835bfd005ca1b
deulos <ragnar@deulos.com>
2019-07-15 14:12:38 +0200

    Add tip, warning, and danger containers

--- src/.vuepress/theme/index.js
+++ src/.vuepress/theme/index.js
@@ -3,5 +3,14 @@ module.exports = (options, ctx) => ({
   plugins: [
     ['@vuepress/active-header-links', options.activeHeaderLinks],
     '@vuepress/plugin-nprogress',
+    ['container', {
+      type: 'tip',
+    }],
+    ['container', {
+      type: 'warning',
+    }],
+    ['container', {
+      type: 'danger',
+    }],
   ]
 });
