54198b6f4216dbbc4a358c0bac75f90b71e32a79
deulos <no@spam.com>
2020-03-07 21:33:22 +0100

    Update docs with 6.0.2 version

--- docs/assets/js/167.8be23a65.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[167],{11:function(n,w,o){}}]);
---
+++ docs/assets/js/167.b4a17c58.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[167],{146:function(n,w,o){}}]);
