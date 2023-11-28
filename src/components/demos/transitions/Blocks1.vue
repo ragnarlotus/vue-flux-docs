d6a0f71f0117129bea058dcfab1eff78db4851ac
deulos <no@spam.com>
2023-11-28 06:47:55 +0100

    feat: demo for transition Blocks1 finished

--- src/components/demos/transitions/Blocks1.vue
+++ src/components/demos/transitions/Blocks1.vue
@@ -1,3 +1,130 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { computed, shallowReactive } from 'vue';
+	import Prism from 'prismjs';
+	import Slider from '@vueform/slider';
+	import { Blocks1 } from 'vue-flux';
+	import Demo from '../../Demo.vue';
+	import {
+		easings,
+		minTileDuration,
+		maxTileDelay,
+		maxTileDuration,
+		minTileDelay,
+	} from '../../../shared/constants';
 
-<template></template>
+	const options = {
+		autohideTime: 0,
+	};
+
+	const transitions = shallowReactive([
+		{
+			component: Blocks1,
+			options: shallowReactive({
+				rows: 0,
+				cols: 8,
+				tileDuration: 300,
+				tileDelay: 1000,
+				easing: 'linear',
+			}),
+		},
+	]);
+
+	const complements = {
+		preloader: true,
+		controls: true,
+	};
+
+	const sourceJs = computed(() => {
+		let code = `
+import {
+	Blocks1,
+} from 'vue-flux';
+
+const transitions = shallowReactive([
+	{
+		component: Blocks1,
+		options: shallowReactive({
+			rows: ${transitions[0].options.rows},
+			cols: ${transitions[0].options.cols},
+			tileDuration: ${transitions[0].options.tileDuration},
+			tileDelay: ${transitions[0].options.tileDelay},
+			easing: '${transitions[0].options.easing}',
+		}),
+	},
+]);`;
+
+		return Prism.highlight(
+			code.trim(),
+			Prism.languages.javascript,
+			'javascript'
+		);
+	});
+</script>
+
+<template>
+	<div class="demo">
+		<Demo
+			:options="options"
+			:transitions="transitions"
+			:numRscs="12"
+			:complements="complements"
+		/>
+
+		<h3>Options</h3>
+
+		<label>
+			<span>Rows:</span>
+			<Slider
+				v-model="transitions[0].options.rows"
+				:min="0"
+				:max="20"
+				@slide="(newRows: number) => (transitions[0].options.rows = newRows)"
+			/>
+		</label>
+
+		<label>
+			<span>Columns:</span>
+			<Slider
+				v-model="transitions[0].options.cols"
+				:min="2"
+				:max="20"
+				@slide="(newCols: number) => (transitions[0].options.cols = newCols)"
+			/>
+		</label>
+
+		<label>
+			<span>Tile duration:</span>
+			<Slider
+				v-model="transitions[0].options.tileDuration"
+				:min="minTileDuration"
+				:max="maxTileDuration"
+				@slide="(newTileDuration: number) => (transitions[0].options.tileDuration = newTileDuration)"
+			/>
+		</label>
+
+		<label>
+			<span>Tile delay:</span>
+			<Slider
+				v-model="transitions[0].options.tileDelay"
+				:min="minTileDelay"
+				:max="maxTileDelay"
+				@slide="(newTileDelay: number) => (transitions[0].options.tileDelay = newTileDelay)"
+			/>
+		</label>
+
+		<label>
+			<span>Easing:</span>
+			<select v-model="transitions[0].options.easing">
+				<option v-for="easing in easings" :key="easing">
+					{{ easing }}
+				</option>
+			</select>
+		</label>
+
+		<h3>Source</h3>
+
+		<div class="language-javascript" data-ext="js">
+			<pre v-html="sourceJs" class="language-javascript" />
+		</div>
+	</div>
+</template>
