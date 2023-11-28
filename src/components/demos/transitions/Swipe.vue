b22b1e2fdd26198596d7db90ea8bc7d5d5505d9f
deulos <no@spam.com>
2023-11-28 10:21:10 +0100

    feat: demo for transition Swipe finished

--- src/components/demos/transitions/Swipe.vue
+++ src/components/demos/transitions/Swipe.vue
@@ -1,3 +1,92 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { computed, shallowReactive } from 'vue';
+	import Prism from 'prismjs';
+	import Slider from '@vueform/slider';
+	import { Swipe } from 'vue-flux';
+	import Demo from '../../Demo.vue';
+	import {
+		easings,
+		minDuration,
+		maxDuration,
+	} from '../../../shared/constants';
 
-<template></template>
+	const options = {
+		autohideTime: 0,
+	};
+
+	const transitions = shallowReactive([
+		{
+			component: Swipe,
+			options: shallowReactive({
+				totalDuration: 1400,
+				easing: 'ease-in-out',
+			}),
+		},
+	]);
+
+	const complements = {
+		preloader: true,
+		controls: true,
+	};
+
+	const sourceJs = computed(() => {
+		let code = `
+import {
+	Swipe,
+} from 'vue-flux';
+
+const transitions = shallowReactive([
+	{
+		component: Swipe,
+		options: shallowReactive({
+			totalDuration: ${transitions[0].options.totalDuration},
+			easing: '${transitions[0].options.easing}',
+		}),
+	},
+]);`;
+
+		return Prism.highlight(
+			code.trim(),
+			Prism.languages.javascript,
+			'javascript'
+		);
+	});
+</script>
+
+<template>
+	<div class="demo">
+		<Demo
+			:options="options"
+			:transitions="transitions"
+			:numRscs="12"
+			:complements="complements"
+		/>
+
+		<h3>Options</h3>
+
+		<label>
+			<span>Total duration:</span>
+			<Slider
+				v-model="transitions[0].options.totalDuration"
+				:min="minDuration"
+				:max="maxDuration"
+				@slide="(newDuration: number) => (transitions[0].options.totalDuration = newDuration)"
+			/>
+		</label>
+
+		<label>
+			<span>Easing:</span>
+			<select v-model="transitions[0].options.easing">
+				<option v-for="easing in easings" :key="easing">
+					{{ easing }}
+				</option>
+			</select>
+		</label>
+
+		<h3>Source</h3>
+
+		<div class="language-javascript" data-ext="js">
+			<pre v-html="sourceJs" class="language-javascript" />
+		</div>
+	</div>
+</template>
